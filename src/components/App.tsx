b567a20b0dc3d19b0e0cc897989c96b5e58df29e
Alexandru Florea <alexflorea16000@gmail.com>
2022-03-19 12:53:17 +0200

    Nav links tweak

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,32 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import '../style/App.css';
 
 function App() {
+
+  const location = useLocation();
+
+  const hasFirstPathSegment = (segment: string): boolean => {
+    return location.pathname.split('/')[1] === segment;
+  }
+
   return (
     <div className='app-component'>
       <div className='nav shadow'>
-        <div className='center'>
-          <Link className='link' to='/articles'>Blog</Link>
+        <div className={hasFirstPathSegment('articles') ? 'center' : 'right'}>
+          <Link
+            className='link'
+            to='/articles'
+          >
+            Blog
+          </Link>
         </div>
-        <div className='right'>
-          <Link className='link' to='/me'>About Me</Link>
+        <div className={hasFirstPathSegment('me') ? 'center' : 'right'}>
+          <Link
+            className='link'
+            to='/me'
+          >
+            About Me
+          </Link>
         </div>
       </div>
       <Outlet />
