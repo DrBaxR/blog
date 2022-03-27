db82f11bcc0963e980e47eefd11472adc4feeafa
Alexandru Florea <alexflorea16000@gmail.com>
2022-03-27 17:12:16 +0300

    Code area

--- src/components/article/Welcome.tsx
+++ src/components/article/Welcome.tsx
@@ -10,22 +10,26 @@ export const Welcome: React.FC = () => {
   return (
     <div className='content'>
       <p>
-        Welcome to my blog! This first post is more of a test where I will include all the types of areas that are going to be used within this site.
+        Welcome to my blog! This first post is more of a test where I will include all the types of areas/styles that are going to be used within this site to indicate certain things.
+        Stuff will get added here as I figure out I need it.
       </p>
       <p>
         With that out of the way, let me show you some of the things that you might encounter while reading some of the articles here.
       </p>
       <Section title='Section'>
         <p>
-          This one is something like a container - it holds other things in it. It will probably be present in all the articles, used as a way to markthe beginning of a
+          This one is something like a container - it holds other things in it. It will probably be present in all the articles, used as a way to mark the beginning of a
           new chapter.
         </p>
       </Section>
       <Section title='Code Area'>
-        <p>An area that is going to hold <code>code</code> in it, not much more to say about this one... Here{'\''}s an example:</p>
+        <p>An area that is going to hold code in it, not much more to say about this one... Here{'\''}s an example:</p>
         <CodeArea
           code={codeSample}
         />
+      </Section>
+      <Section title='Extra Information Areas'>
+        <p>Red, green and yellow areas that hold warnings, extra information and tips (or something like that, didn{'\''}t decide how I{'\''}m going to use them yet).</p>
       </Section>
     </div>
   );
--- src/components/common/CodeArea.tsx
+++ src/components/common/CodeArea.tsx
@@ -1,44 +1,19 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { a11yLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 import './CodeArea.css';
 
 interface Props {
-  // NOTE: indentation needs to be done via spaces, not tabs
   code: string,
 }
 
 export const CodeArea: React.FC<Props> = ({ code }) => {
-  const tabCharacter = '&nbsp;&nbsp;';
-
-  const indentForHTML = (line: string): string => {
-    let newLine = '';
-    let oldLine = line;
-
-    while (oldLine.startsWith(' ')) {
-      oldLine = oldLine.substring(1);
-      newLine += tabCharacter;
-    }
-
-    newLine += oldLine;
-
-    return newLine;
-  };
-
-  const getCodeAsElements = (code: string): ReactElement[] => {
-    return code
-      .split('\n')
-      .map((line, index) => (
-        <div
-          key={`line-${index}`}
-          dangerouslySetInnerHTML={{ __html: indentForHTML(line) }}
-        ></div>
-      ));
-  };
-
-  // TODO: Add highlighting for keywords and stuff
   return (
     <div className='code-area-component'>
-      {getCodeAsElements(code)}
+      <SyntaxHighlighter language='javascript' style={a11yLight}>
+        {code}
+      </SyntaxHighlighter>
     </div>
   );
 };
